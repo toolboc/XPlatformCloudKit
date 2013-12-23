ffbb8abd27fa44df6525dd155866e27205330365
toolboc <toolboc@hotmail.com>
2013-12-23 15:12:15 -0600

    Add warning concerning Ad Control

--- XPlatformCloudKit/XPlatformCloudKit.PCL/AppSettings.cs
+++ XPlatformCloudKit/XPlatformCloudKit.PCL/AppSettings.cs
@@ -194,6 +194,7 @@ namespace XPlatformCloudKit
         //See: http://msdn.microsoft.com/en-us/library/advertising-windows-pubcenter(v=msads.10).asp
         //for information on configuring and providing your own ads
         //Supports 728x90 ads only!  Make sure your add unit matches this size when creating in Pubcenter!
+        //Be careful your ad does not block your Application Title!  To change the render size see lines 65 & 66 of XPlatformCloudKit.Win8/Views/ItemsShowcaseView.xaml.cs
         public const string PubcenterApplicationIdWin8 = "d25517cb-12d4-4699-8bdc-52040c712cab";
         public const string PubcenterAdUnitIdWin8 = "10042999";
         #endregion
