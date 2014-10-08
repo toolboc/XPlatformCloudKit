f40b6793d8e0ee75579a47f1f449583bd3671d9e
Paul DeCarlo <pdecarlo@microsoft.com>
2014-10-08 23:07:07 +0000

    Add Unit Tests for Services

---
+++ XPlatformCloudKit/XPlatformCloudKit.Tests/RssServiceTest.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using XPlatformCloudKit.Models;
+using XPlatformCloudKit.DataServices;
+using System.Threading.Tasks;
+
+namespace XPlatformCloudKit.Tests
+{
+    [TestClass]
+    public class RssServiceTest
+    {
+        [TestMethod]
+        public async Task ValidateRssService()
+        {
+            //If value == -1, ensures all items are fetched
+            AppSettings.RssMaxItemsPerFeed = -1;
+            AppSettings.RssAddressCollection = 
+                new UrlSource[] {
+                                    new UrlSource() { Url = "http://reddit.com/.rss", Group = "Reddit" },
+                                    new UrlSource() { Url = "http://reddit.com/r/technology/.rss", Group = "Reddit Technology" }
+            };
+            var rssService = new RssService();
+
+            var items = await rssService.GetItems();
+
+            Assert.AreEqual(50, items.Count);
+        }
+    }
+}
---
+++ XPlatformCloudKit/XPlatformCloudKit.Tests/TwitterServiceTest.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Threading.Tasks;
+using XPlatformCloudKit.Models;
+using XPlatformCloudKit.DataServices;
+using XPlatformCloudKit.Services;
+using XPlatformCloudKit.Tests.Services;
+using AsyncOAuth;
+using System.Security.Cryptography;
+
+namespace XPlatformCloudKit.Tests
+{
+    [TestClass]
+    public class TwitterServiceTest
+    {
+
+        [ClassInitialize]
+        public static void ClassInitialize(TestContext t)
+        {
+            //Oauth Init
+            OAuthUtility.ComputeHash = (key, buffer) => { using (var hmac = new HMACSHA1(key)) { return hmac.ComputeHash(buffer); } };
+        }
+
+        [TestMethod]
+        public async Task ValidateTwitterService()
+        {
+            ServiceLocator.MessageService = new TestMessageService();
+            //Created @ https://apps.twitter.com, found under "manage api keys"
+            AppSettings.TwitterConsumerKey = "";
+            AppSettings.TwitterConsumerSecret = "";
+            //In "manage api keys" scroll down to create access tokens
+            AppSettings.TwitterAccessToken = "";
+            AppSettings.TwitterAccessSecret = "";
+            AppSettings.TwitterAddressCollection = new UrlSource[]  
+            {
+                 new UrlSource {Url = "https://api.twitter.com/1.1/statuses/user_timeline.json?user_id=pjdecarlo", Group = "PJDeCarlo"}
+            };
+            var twitterService = new TwitterService();
+
+            var items = await twitterService.GetItems();
+
+            Assert.IsTrue(items.Count > 0, "Error: Zero items retrieved from Twitter Service");
+        }
+    }
+}
