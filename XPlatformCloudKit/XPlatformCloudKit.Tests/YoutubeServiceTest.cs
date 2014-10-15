5c1dc7baeefe5cdcce498a5319c91801a7dc3178
P.D <thuyphuong9000@gmail.com>
2014-10-15 19:33:12 +0000

    Update YoutubeServiceTest.cs
    Add Public API key

--- XPlatformCloudKit/XPlatformCloudKit.Tests/YoutubeServiceTest.cs
+++ XPlatformCloudKit/XPlatformCloudKit.Tests/YoutubeServiceTest.cs
@@ -14,7 +14,7 @@ namespace XPlatformCloudKit.Tests
         {
             //Created @ https://code.google.com/apis/console, enable YouTube Data API v3 under APIs then click Credentials
             //Under Public API Access - click "Create New Key"
-            AppSettings.YoutubePublicAPIKey = "";
+            AppSettings.YoutubePublicAPIKey = "AIzaSyCfyarRgBTEHzpC1IaTqdjhen01rRBR-ow";
             AppSettings.YoutubeAddressCollection = new UrlSource [] 
             {
                  new UrlSource {Url = "https://www.googleapis.com/youtube/v3/playlistItems?playlistId=PL0OTHVsGLN2medBm9k-VFtB6cvUbP9n6B&part=snippet&maxResults=50&fields=items%2CnextPageToken&key=" + AppSettings.YoutubePublicAPIKey, Group = "Youtube Playlist"}
@@ -24,6 +24,8 @@ namespace XPlatformCloudKit.Tests
             var items = await youtubeService.GetItems();
 
             Assert.IsTrue(items.Count > 0, "Error: Zero items retrieved from Twitter Service");
+            Assert.AreEqual(200, items.Count);
+            Assert.AreEqual("Candy Crush Saga Level 1", items[0].Title);
         }
     }
 }
